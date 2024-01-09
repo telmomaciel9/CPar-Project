a5051086008fca2bba4300fc0b94222060a7a8be
Telmo Maciel <telmomaciel9@gmail.com>
2024-01-09 02:48:51 +0000

    correção

--- TP3/runcuda.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-#
-#SBATCH --time=01:00             # allocation for 1 minute
-#SBATCH --partition=cpar     # partition (queue)
-#SBATCH --constraint=k20   # allocation of 40 CPUs
-
-nvprof ./bin/MDpar_cuda < inputdata.txt
