a121cf8f0cc97c83b7ab76cf6605c7814afd5c84
Telmo Maciel <telmomaciel9@gmail.com>
2024-01-09 03:30:24 +0000

    correção

---
+++ TP3/test.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+#SBATCH --time=00:10:00
+#SBATCH --partition=cpar
+#SBATCH --exclusive
+
+
+threads=(1 2 4 8 16 32 40)
+
+
+for nthreads in "${threads[@]}"
+do
+	export OMP_NUM_THREADS=${nthreads}
+	echo ${OMP_NUM_THREADS}
+	time `./MDpar.exe <inputdata.txt >lixo`
+done
