f78197483778fa0d696ad762419131d4ddce0ba9
telmomaciel9 <telmomaciel9@gmail.com>
2024-01-10 00:56:43 +0000

    correçao script

--- TP3/runcuda.sh
+++ TP3/runcuda.sh
@@ -4,4 +4,7 @@
 #SBATCH --partition=cpar     # partition (queue)
 #SBATCH --constraint=k20   # allocation of 40 CPUs
 
+module load gcc/7.2.0
+module load cuda/11.3.1
+
 time nvprof ./bin/MDpar_cuda < inputdata.txt
